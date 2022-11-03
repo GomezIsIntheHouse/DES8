4cbac6b198667aaffbc1e1dec25888c8a219cdf9
Julian Gomez <julian1995ag@gmail.com>
2022-11-03 14:04:20 -0300

    avances juev

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ const PORT = process.env.PORT || 4000
 app.set('port', PORT)
 
 app.listen(app.get('port'), ()=>{
-    console.log('Server READY in PORT:' +app.get('port'))
+    console.log('Server READY in PORT:' + app.get('port'))
 })
 
